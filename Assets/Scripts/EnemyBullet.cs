a917e1fa714948095f977a6625d70d32ba34dbec
kimyoungsun <kyssmart@naver.com>
2018-09-05 00:29:08 +0900

    111
    111

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+	public float moveSpeed = 4f;
+	Rigidbody rb;
+	Transform trans;
+	public float tumble = 10f;
+	public GameObject prefabHitExplosion;
+	public GameObject prefabDieExplosion;
+
+	void Start () {
+		trans = transform;
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//아래 방향으로 이동하기..(회전축이 변화해서 World공간을 기준으로 이동)
+		trans.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);		
+	}
+
+	void OnTriggerEnter(Collider _col){		
+		if (_col.CompareTag ("Boundary")) {
+			//아래 경계라인에 충돌..
+			OnDestroy();
+		} else if (_col.CompareTag ("Player")) {
+			//플레이어와 충돌..
+			Instantiate(prefabDieExplosion, trans.position, trans.rotation);
+
+			PlayerController _scpPlayer = _col.GetComponent<PlayerController> ();
+			_scpPlayer.HitDamage(1, trans.position);
+			OnDestroy ();
+		}
+	}
+
+	void OnDestroy(){
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/ZTestPooling/Pooling/Scripts/TestMan.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMan : MonoBehaviour {
+
+	public float fireSpeed = 10;
+	public float moveSpeed = 5;
+	public float turnSpeed = 180;
+	public int cursor = 0;
+	public Transform firePoint; 
+	string[] strNames;
+
+	void Start(){
+		Debug.Log ("1,2 커서가 변경된다.");
+
+		strNames = new string[PoolManager.ins.objList.Count];
+		for (int i = 0; i < PoolManager.ins.objList.Count; i++) {
+			strNames[i] = PoolManager.ins.objList [i].name;
+		}
+	}
+
+	// FPS 총변경처럼 구성...
+	void Update () {
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			cursor--;
+			if (cursor < 0) {
+				cursor = strNames.Length - 1;
+			}
+		} else if (Input.GetKey (KeyCode.Alpha2)) {
+			cursor++;
+			if (cursor >= strNames.Length) {
+				cursor = 0;
+			}
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			//이름을 알면 직접, 전 Pooling에 순서대로...
+			GameObject _obj = PoolManager.ins.Instantiate (strNames[cursor], firePoint.position, firePoint.rotation);
+			Rigidbody _rb = _obj.GetComponent<Rigidbody> ();
+			_rb.velocity = firePoint.forward * fireSpeed;
+		}
+
+		float _h = Input.GetAxis ("Horizontal");
+		float _v = Input.GetAxis ("Vertical");
+		if (Mathf.Abs (_h) > 0.1f || Mathf.Abs (_v) > 0.1f) {
+			transform.Translate (transform.forward * _v * moveSpeed * Time.deltaTime);
+			transform.Rotate(Vector3.up * _h * turnSpeed * Time.deltaTime);
+		}
+	
+	}
+}
